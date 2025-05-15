ce349b7e97d3cd492becb7a01178a4d9f0ecda9d
Molnar-Zoltan <weperdev@gmail.com>
2025-05-16 00:23:43 +0200

    Add config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/little-lemon-reserve-a-table/',
   plugins: [react()],
   test: {
       globals: true,
